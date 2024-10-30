031c76d37d33d9ca702774f10078c17b4b15c1dd
Vishal <vishalonceagain@gmail.com>
2024-10-30 10:11:32 +0000

    added deleted user placeholder

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
@@ -10,6 +10,12 @@ DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///./bug_reports.db')
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+
+@event.listens_for(engine, "connect")
+def enable_sqlite_foreign_keys(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 # Dependency to get DB session
 def get_db():
