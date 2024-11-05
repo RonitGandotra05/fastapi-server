b9326f9a9e2f07dd4ac8dafea5e2acf469ee93ab
Vishal <vishalonceagain@gmail.com>
2024-11-05 15:23:55 +0000

    feat: include user ID in login response

--- routes/users.py
+++ routes/users.py
@@ -126,7 +126,8 @@ def login_for_access_token(
     return {
         "access_token": access_token,
         "token_type": "bearer",
-        "is_admin": user.is_admin
+        "is_admin": user.is_admin,
+        "id": user.id
     }
 
 # Logout Endpoint (Accessible by all authenticated users)
